9a7b90000ae372bdfb2cd19f38f49e69509eac1f
Roberto <roberto.guarneros11@gmail.com>
2024-10-25 19:43:43 +0200

    First Commit

---
+++ main/calc/calc.h
@@ -0,0 +1,4 @@
+#ifndef CALC_H  // used to avoid multiple inclusion of the header file
+#define CALC_H // define the header file
+    int add(int a, int b); // function declaration
+#endif // CALC_H
